51824ecd8e576e5ed9e5a262bbbe68d495ec0fc3
Rafinad <rafinad@outlook.com>
2021-05-10 00:02:15 +0300

    Initial Commit

---
+++ LocalNotificationTest/LocalNotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationTestApp.swift
+//  LocalNotificationTest
+//
+//  Created by Сахар Кубический on 10.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
